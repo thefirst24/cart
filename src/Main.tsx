b58dc4b4b099987195de31afd06cc565e74c3030
Sharipov Aidar <Sharipow777@gmail.com>
2021-06-29 18:47:33 +0500

    second commit

--- src/Main.tsx
+++ src/Main.tsx
@@ -9,13 +9,17 @@ interface Phone {
     price: number
 }
 
-interface Item {
-
+interface ListItem {
+    phone: Phone,
+    incr: Function,
+    dicr: Function,
+    destroy: Function
 }
 
-const Item: FC<Phone> = ({ img, title, price }) => {
+const Item: FC<ListItem> = (item) => {
+    const {img,title,price} = item.phone;
     const state = useContext(MainContext);
-    const count = state.items.find(item => item.phone.title == title)?.count;
+    const count = state.items.find(item => item.phone.title === title)?.count;
     return (
     <div className="item">
         <img src={img} alt={title}></img>
@@ -25,26 +29,26 @@ const Item: FC<Phone> = ({ img, title, price }) => {
             <button>remove</button>
         </div>
         <div  className="count">
-            <button><img src="up.svg" alt="increase" /></button>
+            <button onClick={() => item.incr()}><img src="up.svg" alt="increase" /></button>
             <h4>{count}</h4>
-            <button><img src="bottom.svg" alt="descrease" /></button>
+            <button onClick={() => item.dicr()}><img src="bottom.svg" alt="descrease" /></button>
         </div>
     </div>
     )
 }
 
-const MainFooter = () => {
+const MainFooter : FC<any> = (clearAll) => {
     const state = useContext(MainContext);
     let price = 0;
-    state.items.map((item) => price += item.phone.price * item.count);
+    state.items.map((item) => price += item?.phone?.price * item?.count);
     return (
         <div className="main-footer">
             <div className="line"></div>
             <div className="total">
                 <h4>Total</h4>
-                <h4>${price}</h4>
+                <h4>${Math.round(price * 100) / 100}</h4>
             </div>
-            <button className="clear">CLEAR CART</button>
+            <button className="clear" onClick={() => clearAll.clearAll()}>CLEAR CART</button>
         </div>
 
     )
@@ -72,10 +76,10 @@ const initialState = {
     ]
 }
 
-type StateItem = {
+interface StateItem {
     phone:Phone,
     count: number
-}
+} 
 
 type State = {
     items : Array<StateItem>
@@ -83,29 +87,63 @@ type State = {
 
 type Action = 
     | {type: "incr",title:string} 
-    | {type: "dicr",title: string}
+    | {type: "decr",title: string}
+    | {type: "destroy",title: string}
+    | {type: "clear"}
 
-function reducer(state : State,action : Action){
-    return state;
-}
+
 
 export default function Main() {
+    
+    
+    const reducer = React.useCallback((state : State,action : Action) => {
+        let newState = {...state}
+        if (action.type === "clear") {
+            newState.items = [];
+            return newState;
+        }
+        let item = newState.items.find(item => item?.phone.title === action.title);
+        const index = newState.items.indexOf(item!);
+        switch (action.type) {
+            case 'incr': 
+                newState.items[index].count += 0.5;
+                return newState;
+        
+            case 'decr':
+                newState.items[index].count -= 0.5;
+                return newState;
+            case 'destroy':
+                newState.items = newState.items.filter(item => item.phone.title !== action.title);
+                return newState;
+            default:
+                return newState;
+        }
+    },[])
+
+
     const [state,dispatch] = useReducer(reducer,initialState)
+    console.log(state);
     return (
         <MainContext.Provider value={state}>
             <main>
                 <h1>YOUR BAG</h1>
                 <div className="items">
                     {state.items.map(item => 
-                    <Item 
-                        img={item.phone.img} 
-                        title={item.phone.title} 
-                        price={item.phone.price} 
-                        key={Math.random() * 10}
+                    {
+                        if (item.count <= 0) dispatch({type:"destroy",title:item.phone.title})
+                        else
+                        return <Item 
+                            phone={item.phone} 
+                            incr={() => dispatch({type:"incr",title:item.phone.title})} 
+                            dicr={() => dispatch({type:"decr",title:item.phone.title})}
+                            destroy={() => dispatch({type:"destroy",title:item.phone.title})}
+                            key={Math.random() * 10}
                         />
+                    }
                     )}
                 </div>
-                <MainFooter />
+                {state.items.length != 0 && <MainFooter clearAll={() => dispatch({type:"clear"})}/>}
+                {state.items.length == 0 && <h2>Is currently empty</h2>}
             </main>
         </MainContext.Provider>
     )
